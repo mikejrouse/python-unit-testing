26de7ee17fb50c603db0c3b6030998f60a4625fd
michael-r <mike.j.rouse@emcins.com>
2023-10-24 08:54:05 -0500

    RE-503: unit test negative input should throw value error

--- part2/test_calculator.py
+++ part2/test_calculator.py
@@ -1,4 +1,5 @@
 import pytest
+import unittest
 from .calculator import calculator
 
 #Example Positive Test
@@ -10,3 +11,8 @@ def test_one_hundred_plus_ninety_nine_should_return_one_hundred_ninety_nine():
 
 def test_calculator_with_multiple_integers():
     assert calculator(6, 7, 18, 25) == 56
+
+#Example Negative input throwing an Error
+def test_negative_input_should_throw_error(self):
+    with self.assertRaises(ValueError):
+        calculator(6, 7, -18, 25)
